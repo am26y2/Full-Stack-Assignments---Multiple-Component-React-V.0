fb28480946445ddd62d6057acfc66332d21434a2
Amit Raj <amitraj9046@gmail.com>
2020-10-21 16:31:15 +0530

    Multiple Component React

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,15 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-            <>
-               
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <p title="project-name"></p>
+        <p title="project-description"></p>
+      </>
+    );
+  }
 }
-
 
 export default App;
